e6d742c14243aea92847065d4477d0ec46e91a69
Ruchika Adak <adakruchika5@gmail.com>
2026-09-04 08:52:49 +0530

    feat: add solutions for sl_8, sl_9, and sl_10

---
+++ sl_10/Armstrong.java
@@ -0,0 +1,16 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        int n = 153, temp = n, sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n /= 10;
+        }
+
+        if (sum == temp)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not an Armstrong Number");
+    }
+}
---
+++ sl_8/ASCII.java
@@ -0,0 +1,7 @@
+public class ASCII {
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        System.out.println("ASCII value: " + (int) ch);
+    }
+}
